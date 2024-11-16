87a8b9f05adcf7c0135207a82a13e83193649dd2
Shikhar Pandey <100460904+ShikharPandey123@users.noreply.github.com>
2024-11-16 21:39:49 +0530

    Update http.js

--- src/http.js
+++ src/http.js
@@ -1,30 +1,8 @@
 export default async function fetchAvailableMeals(){
-    const response = await fetch('http://localhost:4000/meals')
+    const response = await fetch('https://food-order-web-app-2.onrender.com/meals')
     const resData= await response.json();
     if(!response.ok){
         throw new Error('Failed to fetch places');
     }
     return resData;
 }
-// export async function fetchOrders(){
-//     const response = await fetch('http://localhost:4000/orders')
-//     const resData= await response.json();
-//     if(!response.ok){
-//         throw new Error('Failed to fetch orders');
-//     }
-//     return resData.orders;
-// }
-// export async function updateOrders(places){
-//     const response=await fetch('http://localhost:4000/orders',{
-//         method: 'PUT',
-//         body: JSON.stringify({places}),
-//         headers:{
-//             'Content-Type':'application/json',
-//         }
-//     });
-//     const resData=await response.json();
-//     if(!response.ok){
-//         throw new Error("Failed to update user data");
-//     }
-//     return resData.message;
-// }
